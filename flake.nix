f537f54cae5c88ce2cd386642aa1a4e20600b0bc
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2025-10-26 20:31:05 -0400

    Add flake.

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages.default = pkgs.coqPackages.coq-cast;
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+        overlays.default = final: prev: {
+          coqPackages = prev.coqPackages.overrideScope (final: prev: {
+            coq-cast = prev.mkCoqDerivation {
+              pname = "coq-cast";
+              defaultVersion = "dev";
+              release.dev.src = ./.;
+              propagatedBuildInputs = [
+                final.coq
+                final.mathcomp.ssreflect
+              ];
+            };
+          });
+        };
+      };
+    };
+}
